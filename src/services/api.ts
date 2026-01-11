e43b5408996bcde95e312cb3297a14f398500624
Wesley Eduardo <wesleyeduardo.dev@gmail.com>
2026-01-11 10:58:39 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const API_URL = 'https://e3ab272488a7.ngrok-free.app/api';
+const API_URL = ' https://189d884687ed.ngrok-free.app/api';
 
 export const api = axios.create({
   baseURL: API_URL,
